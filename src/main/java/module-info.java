c61ec92db03715d0986e99fc3932fbb195e82e59
TristanSchwekendiek <tristanschwekendiek@gmail.com>
2023-11-16 03:10:51 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp1011assignment21207799 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp1011assignment21207799 to javafx.fxml;
+    exports com.example.comp1011assignment21207799;
+}
